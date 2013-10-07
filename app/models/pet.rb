cc05d73510f3cf25114cfd9adf77be171727afeb
bastova <abastov@gmail.com>
2013-10-07 19:58:37 -0400

    changed migration

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,5 @@
 class Pet < ActiveRecord::Base
 	attr_accessible :name, :age, :user_id
-	self.inheritance_column = nil
 	belongs_to :user
 	validates_presence_of :name
 end
--- db/migrate/20130929021637_create_users.rb
+++ db/migrate/20130929021637_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password
+	  t.has_many :pets
 
       t.timestamps
     end
