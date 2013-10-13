737e52548774dff55f6088cfd540ab2794ec1f03
bastova <abastov@gmail.com>
2013-10-13 18:52:10 -0400

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
 	has_many :pets
 	attr_accessible :name, :email, :password
 	belongs_to :guild
-	validates_presence_of :name
+	validates :name, presence: true
+	validates :password, length: {minimum: 6}
+	emailFormatValidation = /[a-z.-]+@[a-z \d -.]+[.][a-z]+/i 
+	validates :email, format: {with: emailFormatValidation}
+	validates :email, uniqueness: true
+	validates :password, confirmation: true
+
 end
