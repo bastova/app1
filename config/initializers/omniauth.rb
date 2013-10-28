8e9497d1b854997843c1ee7d61858ec50fa2e637
bastova <abastov@gmail.com>
2013-10-28 18:28:21 -0400

    facebook login 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   
 end
