9550c4b69c134627824a5ca767e96b26e6018a79
bastova <abastov@gmail.com>
2013-10-28 18:54:38 -0400

    bla

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   
   if Rails.env.development?
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9'
+    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
   else
-    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => '', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   end
 end
