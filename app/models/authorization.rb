a908c6ba6f73ef34c798bcc12c9c1ac884d2ea6c
bastova <abastov@gmail.com>
2013-10-28 20:40:16 -0400

    a

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,12 +4,17 @@ class Authorization < ActiveRecord::Base
 	validates :provider, :uid, :presence => true
 	
 	def self.find_or_create(auth_hash)
-	  unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+	  @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+	  if @authorization
+		render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+	  else
 		user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
 		user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 		user.save
+	 
+		render :text => "Hi #{user.name}! You've signed up."
 	  end
 	 
-	  auth
+	  auth = @authorization
 	end
 end
