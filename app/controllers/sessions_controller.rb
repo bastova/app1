dcc05ea1447e1fdccd7c637185040d8842cfbad5
bastova <abastov@gmail.com>
2013-10-28 20:29:33 -0400

    back to multi auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
+=begin
   def create
 	  auth_hash = request.env['omniauth.auth']
 	 
@@ -17,6 +18,25 @@ class SessionsController < ApplicationController
 	  end
 	  session[:user_id] = @authorization.user.id
 	end
+=end
+def create
+  auth_hash = request.env['omniauth.auth']
+ 
+  if session[:user_id]
+    # Means our user is signed in. Add the authorization to the user
+    User.find(session[:user_id]).add_provider(auth_hash)
+ 
+    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+  else
+    # Log him in or sign him up
+    auth = Authorization.find_or_create(auth_hash)
+ 
+    # Create the session
+    session[:user_id] = auth.user.id
+ 
+    render :text => "Welcome #{auth.user.name}!"
+  end
+end
 
   def failure
 	  render :text => "Sorry, but you didn't allow access to our app!"
