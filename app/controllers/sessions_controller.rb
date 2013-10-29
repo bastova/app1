2e9320e89444c4524e32bb34686d6d42fff9a052
bastova <abastov@gmail.com>
2013-10-28 20:44:12 -0400

    b

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,12 +22,7 @@ class SessionsController < ApplicationController
 def create
   auth_hash = request.env['omniauth.auth']
  
-  if session[:user_id]
-    # Means our user is signed in. Add the authorization to the user
-    User.find(session[:user_id]).add_provider(auth_hash)
- 
-    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
-  else
+  
     # Log him in or sign him up
     auth = Authorization.find_or_create(auth_hash)
  
@@ -35,7 +30,6 @@ def create
     session[:user_id] = auth.user.id
  
     render :text => "Welcome #{auth.user.name}!"
-  end
 end
 
   def failure
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,17 +4,11 @@ class Authorization < ActiveRecord::Base
 	validates :provider, :uid, :presence => true
 	
 	def self.find_or_create(auth_hash)
-	  @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-	  if @authorization
-		render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
-	  else
-		user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
-		user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-		user.save
-	 
-		render :text => "Hi #{user.name}! You've signed up."
+	  unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+		user = User.create :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
+		auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 	  end
 	 
-	  auth = @authorization
+	  auth
 	end
 end
