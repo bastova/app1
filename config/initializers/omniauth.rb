3ed7d70f4db2fb09d0df343e5b29ce6a07ec9e68
bastova <abastov@gmail.com>
2013-10-28 22:52:06 -0400

    login attempt 11212100

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
     provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
   else
-    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => '', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+    provider :facebook, '639725102717284', 'd342f594c8ebc05da5a0fcb84ae9b6d9', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   end
 end
