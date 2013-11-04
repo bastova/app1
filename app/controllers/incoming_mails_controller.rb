8d2fb530c7a1af5126b7a7b600214ef2b0b5b202
bastova <abastov@gmail.com>
2013-11-03 21:16:12 -0500

    cloudmailin 4

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -11,9 +11,4 @@ class IncomingMailsController < ApplicationController
 
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
-  
-  def index  
-	  
-  end
-  
 end
