b49da86b97dc5f6cb4de225833c1ee67b9f58a7a
bastova <abastov@gmail.com>
2013-11-17 18:35:52 -0500

    more jquery

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -3,8 +3,17 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(
-	
 	function(){
+		$('.myclass').hide();
+	
+		$('.text').hover(function(){
+			$(this).css('color','purple');
+			$(this).css('cursor','pointer');
+		});
+		$('.text').click(function(){
+			$('.myclass').toggle();
+		});
+	
 		$('.myclass').hover(function(){
 			$(this).css('background','green');
 		});
